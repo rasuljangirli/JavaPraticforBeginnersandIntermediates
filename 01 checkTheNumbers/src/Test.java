1040b22026262a1990238d5c172fd75a70da8840
rasuljangirli <resulcahangirli99@gmail.com>
2024-10-04 13:26:18 +0400

    one project

---
+++ 01 checkTheNumbers/src/Test.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		numbers();
+	}
+
+	@SuppressWarnings("resource")
+	public static void numbers() {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Welcome");
+		System.out.print("How many elements should the array contain?");
+		
+		int numberLengt = scanner.nextInt();
+		
+		int[] numbers = new int[numberLengt];
+		for (int i = 0; i < numberLengt; i++) {
+			System.out.print(" Plase enter the number " + i + "--------");
+			int number = scanner.nextInt();
+			numbers[i] = number;
+		}
+		testNumber(numbers);
+	}
+
+	public static void testNumber(int numbers[]) {
+		for (int number : numbers) {
+			if (number % 2 == 0) {
+				System.out.println(" is an even number " + number);
+			} else {
+				System.out.println(" is an odd number " + number);
+			}
+		}
+	}
+
+}
